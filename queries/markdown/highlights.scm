aafe99c8ef3de3ac1becdb46305c637ff24d282c
loqusion <38332081+loqusion@users.noreply.github.com>
2024-01-19 03:30:32 -0600

    feat(highlights): more nospell

--- queries/markdown/highlights.scm
+++ queries/markdown/highlights.scm
@@ -1,3 +1,13 @@
 ; extends
+((inline) @_inline
+ (#lua-match? @_inline "^{")
+ (#lua-match? @_inline "^}")) @nospell
+
+((inline) @_inline (#lua-match? @_inline "^%s*const")) @nospell
+((inline) @_inline (#lua-match? @_inline "^%s*let")) @nospell
+((inline) @_inline (#lua-match? @_inline "^%s*var")) @nospell
+
 ((inline) @_inline (#lua-match? @_inline "^%s*import")) @nospell
 ((inline) @_inline (#lua-match? @_inline "^%s*export")) @nospell
+
+((inline) @_inline (#lua-match? @_inline "^%s*await")) @nospell
