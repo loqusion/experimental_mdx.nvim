5484af98a546c22da86ad8958022d227f25ea431
loqusion <38332081+loqusion@users.noreply.github.com>
2024-01-19 02:31:27 -0600

    feat(injections): add some queries

--- after/queries/markdown/injections.scm
+++ after/queries/markdown/injections.scm
@@ -1,7 +1,29 @@
 ; extends
+((html_block) @injection.content
+ (#set! injection.language "javascript"))
+
+((inline) @injection.content
+ (#lua-match? @injection.content "^{")
+ (#lua-match? @injection.content "}$")
+ (#set! injection.language "javascript"))
+
+((inline) @injection.content
+ (#lua-match? @injection.content "^%s*const")
+ (#set! injection.language "javascript"))
+((inline) @injection.content
+ (#lua-match? @injection.content "^%s*let")
+ (#set! injection.language "javascript"))
+((inline) @injection.content
+ (#lua-match? @injection.content "^%s*var")
+ (#set! injection.language "javascript"))
+
 ((inline) @injection.content
  (#lua-match? @injection.content "^%s*import")
  (#set! injection.language "javascript"))
 ((inline) @injection.content
  (#lua-match? @injection.content "^%s*export")
+ (#set! injection.language "javascript"))
+
+((inline) @injection.content
+ (#lua-match? @injection.content "^%s*await")
  (#set! injection.language "javascript"))
