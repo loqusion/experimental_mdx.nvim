c55fc802ab3f4dba4c9bce174ef39a44b9575ec4
loqusion <38332081+loqusion@users.noreply.github.com>
2024-01-19 00:42:45 -0600

    feat(ftdetect): init

---
+++ ftdetect/mdx.lua
@@ -0,0 +1,11 @@
+local ft = "mdx"
+local pattern = { "*.mdx" }
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = pattern,
+  callback = function(ev)
+    vim.api.nvim_set_option_value("filetype", ft, {
+      buf = ev.buf,
+    })
+  end,
+})
